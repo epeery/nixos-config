d9e86d93a4b37fa394f69b08aab1dc25f598b879
Eli Peery <eli.g.peery@gmail.com>
2020-04-03 18:57:06 -0400

    Removed eli-home.nix

--- eli-home.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ...}:
-{
-  imports = [
-    (import "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos")
-  ];
-
-  home-manager.users.eli = (import ./home.nix);
-}
